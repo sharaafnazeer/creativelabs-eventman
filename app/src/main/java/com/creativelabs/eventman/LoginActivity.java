b083c34865e3b5d505b3d615233e66e72fe44757
Sharaaf Nazeer <Sharaaf.Nazeer@author-it.com>
2021-10-18 20:09:22 +0530

    Class 02

---
+++ app/src/main/java/com/creativelabs/eventman/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.creativelabs.eventman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+    String userName = "admin";
+    String password = "qwerty";
+
+    // Java int string float char boolean
+    // 2 Edit Text, Button 2, TextView
+
+    EditText etUsername; // Variable declaration
+    EditText etPassword;
+    Button btnLogin;
+    Button btnNewAccount;
+    TextView tvForgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnNewAccount = findViewById(R.id.btnCreateNewAccount);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
+        btnLogin.setOnClickListener(v -> {
+            // Logic
+
+            // Validate username and password
+            // 1. Check for empty or null values
+            // 2 . Check for credentials
+            String uname = etUsername.getText().toString();
+            String pass = etPassword.getText().toString();
+
+            if (validate(uname, pass)) {
+                Intent homeIntent = new Intent(this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        });
+    }
+
+    private boolean validate(String uname, String pass) {
+        if (TextUtils.isEmpty(uname)) {
+            Toast.makeText(this, "Username is empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Password is empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (!(userName.equals(uname) && password.equals(pass))) {
+            Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+}
